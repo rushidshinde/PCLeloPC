ded2cae199adc8c50e80415e065e79e254e71b4d
Rushikesh Shinde <91381429+rushidshinde@users.noreply.github.com>
2022-03-20 09:37:43 +0530

    upload project

---
+++ buildScript.js
@@ -0,0 +1,15 @@
+function mouseOnProcessor(){
+    document.getElementById("advanceMode").style.filter = "blur(4px)";
+}
+
+function mouseOffProcessor(){
+    document.getElementById("advanceMode").style.filter = "blur(0px)";
+}
+
+function mouseOnDemo2(){
+    document.getElementById("easyMode").style.filter = "blur(4px)";
+}
+
+function mouseOffDemo2(){
+    document.getElementById("easyMode").style.filter = "blur(0px)";
+}
